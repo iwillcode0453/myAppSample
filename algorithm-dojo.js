73fa35192eb42068fd71bc2796cd1dbc77cdb702
Erickson O. Dumaran <ericagustin23@gmail.com>
2022-06-28 20:42:01 +0800

    added coding problem and solution

--- algorithm-dojo.js
+++ algorithm-dojo.js
@@ -46,3 +46,47 @@ function extractDigit(num, digit) {
   if (digitNum >= 0)
   return numString[numString.indexOf(".", 0)-1 - digitNum]
 }
+
+==============================================================
+  
+// Problem:
+//The Luhn formula is sometimes used to validate credit card numbers. Create the function isCreditCardValid(digitArr) that accepts an array of digits on the card (13-19 depending on the card), and returns a boolean whether the card digits satisfy the Luhn formula, as follows:
+
+//Set aside the last digit; do not include it in these calculations (until step 5);
+Starting from the back, multiply the digits in odd positions (last, third-to-last, etc.) by 2;
+If any results are larger than 9, subtract 9 from them;
+Add all numbers (not just our odds) together;
+Now add the last digit back in – the sum should be a multiple of 10.
+For example, when given digit array [5,2,2,8,2], after step 1) it becomes [5,2,2,8], then after step 2) it is [5,4,2,16]. Post step 3), we have [5,4,2,7], then following 4) it becomes 18. After step 5) our value is 20, so ultimately we return true. If the final digit were any non-multiple of 10, we would instead return false.
+
+function isCreditCardValid(digitArr) {
+  // Enter code below
+let reverseArr = digitArr.reverse();
+  console.log(reverseArr)
+  for(let i = 1; i < reverseArr.length; i++) {
+    if(i % 2 !== 0) {
+      // 
+      reverseArr[i] *= 2;
+      console.log(reverseArr[i])
+      // if digit in array > 9 subtract it by 9
+      if(reverseArr[i] > 9) {
+        reverseArr[i] -= 9;
+      }
+    }
+  }
+  console.log('this is reverseArr',reverseArr)
+  let sum = reverseArr.reduce((acc, current) => acc + current, 0);
+  console.log('the sum is', sum);
+  if(sum % 10 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+  // return oddDigits    
+}
+  
+  
+  
+  
+  
+  
